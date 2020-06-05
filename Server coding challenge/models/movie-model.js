a84acd6d118d7ef343c223018db5d070d20e89fc
aliciacisnerosm <aliciacisnerosm@gmail.com>
2020-06-05 09:43:44 -0500

    final-exam

--- Server coding challenge/models/movie-model.js
+++ Server coding challenge/models/movie-model.js
@@ -1,49 +1,72 @@
-const mongoose = require( 'mongoose' );
+const mongoose = require('mongoose');
 
 const moviesSchema = mongoose.Schema({
-    movie_ID : {
-        type : Number,
-        unique : true,
-        required : true
+  movie_ID: {
+    type: Number,
+    unique: true,
+    required: true,
+  },
+  movie_title: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  actors: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'actors',
+      required: true,
     },
-    movie_title : {
-        type : String,
-        required : true
-    },
-    year :  {
-        type : Number,
-        required : true
-    },
-    rating : {
-        type : Number,
-        required : true
-    },
-    actors : [{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'actors',
-        required : true
-    }]
+  ],
 });
 
-const moviesCollection = mongoose.model( 'movies', moviesSchema );
+const moviesCollection = mongoose.model('movies', moviesSchema);
 
 const Movies = {
-    createMovie : function( newMovie ){
-        return moviesCollection
-                .create( newMovie )
-                .then( createdMovie => {
-                    return createdMovie;
-                })
-                .catch( err => {
-                    throw new Error( err );
-                });
-    }
-    /*
-        Your code goes here
-    */
-}
-
-module.exports = {
-    Movies
+  createMovie: function (newMovie) {
+    return moviesCollection
+      .create(newMovie)
+      .then((createdMovie) => {
+        return createdMovie;
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  },
+  removeActorFromMovieList: function (movieid, actors) {
+    return moviesCollection
+      .findOneAndUpdate(
+        { id: movieid },
+        { $set: { actors: actors } },
+        { new: true }
+      )
+      .then((result) => {
+        return result;
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  },
+  getMovieByID: function (movieid) {
+    return moviesCollection
+      .findOne({ id: movieid })
+      .then((result) => {
+        console.log(result);
+        return result;
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  },
 };
 
+module.exports = {
+  Movies,
+};
